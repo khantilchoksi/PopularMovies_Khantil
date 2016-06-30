76308d862c76d7102753f248aa9c00396e7c5bcf
Khantil Choksi <khantilchoksi@gmail.com>
2016-06-30 20:04:11 +0530

    Settings Activity Back Button Changes

--- app/src/main/java/com/example/khantilchoksi/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/example/khantilchoksi/popularmovies/SettingsActivity.java
@@ -198,7 +198,9 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
+
+                startActivity(new Intent(getActivity(), MainActivity.class));
+                this.getActivity().finish();
                 return true;
             }
             return super.onOptionsItemSelected(item);
